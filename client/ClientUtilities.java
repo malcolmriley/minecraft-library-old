caa97d78aed6a87c6cf7fba4dde464e6dd5c0411
Malcolm Riley <malcolmriley@users.noreply.github.com>
2021-03-17 08:57:12 -0700

    Implement ClientUtilities.Render

--- client/ClientUtilities.java
+++ client/ClientUtilities.java
@@ -1,10 +1,14 @@
 package paragon.minecraft.library.client;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+
 import net.minecraft.client.gui.IHasContainer;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.RegistryObject;
 
 /**
@@ -15,9 +19,11 @@ import net.minecraftforge.fml.RegistryObject;
 public class ClientUtilities {
 	
 	/**
-	 * Utility class for various client-sided UI utility methods.
+	 * Utility class for various client-sided UI methods.
 	 */
 	public static class UI {
+
+		private UI() { }
 		
 		/**
 		 * Convenience method for binding a {@link ContainerType} {@link RegistryObject} to a {@link ScreenManager.IScreenFactory}.
@@ -38,6 +44,44 @@ public class ClientUtilities {
 		@SuppressWarnings("unchecked") // Unchecked cast of erased parameterized type from RegistryObject for use in ScreenManager.registerFactory, which expects explicitly-matching type parameters
 		protected static <T extends Container> ContainerType<T> castFromRegistryObject(RegistryObject<ContainerType<?>> source) {
 			return (ContainerType<T>)source.get();
+		}
+		
+	}
+	
+	/**
+	 * Utility class for various render-related methods.
+	 */
+	public static class Render {
+		
+		private Render() { }
+		
+		/**
+		 * Method for drawing a simple centered background layer texture, suitable for use as a {@link ContainerScreen}-derived backdrop.
+		 * <p>
+		 * Resets the render color, binds the provided texture, and then blits the texture using the provided {@link MatrixStack}.
+		 * <p>
+		 * The provided {@link ResourceLocation} should be a fully-qualified path with the appropriate suffix.
+		 * 
+		 * @param <S> The {@link Screen} type
+		 * @param screenReference - A reference to the calling {@link Screen} derivative (callers may simply supply {@code this})
+		 * @param currentStack - A reference to the current {@link MatrixStack}
+		 * @param texture - A {@link ResourceLocation} corresponding to the location of the texture to draw
+		 * @param textureWidth - The width of the texture, in pixels
+		 * @param textureHeight - The height of the texture, in pixels
+		 */
+		public static <S extends Screen> void drawCenteredBackgroundLayer(S screenReference, MatrixStack currentStack, ResourceLocation texture, int textureWidth, int textureHeight) {
+			Render.resetRenderColor();
+			screenReference.getMinecraft().getTextureManager().bindTexture(texture);
+			int xPos = (screenReference.width - textureWidth) / 2;
+			int yPos = (screenReference.height - textureHeight) / 2;
+			screenReference.blit(currentStack, xPos, yPos, 0, 0, textureWidth, textureHeight);
+		}
+
+		/* Internal Methods */
+		
+		@SuppressWarnings("deprecation") // RenderSystem.color4f is deprecated, but all vanilla UI uses it to set the render color for bound UI textures.
+		protected static void resetRenderColor() {
+			RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 		}
 		
 	}
